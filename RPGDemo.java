59e1f422c20320331afebd2b56ce31c5f6ac24ea
ytp0938 <163087832+ytp0938@users.noreply.github.com>
2024-04-09 08:23:20 +1200

    Update RPGDemo.java

--- RPGDemo.java
+++ RPGDemo.java
@@ -16,16 +16,21 @@ public class RPGDemo
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws InterruptedException 
     {
         Scanner scan = new Scanner(System.in);
         
         System.out.println("Please enter your name:");
         Player pn = new Player();
         pn.playerName = scan.nextLine();
-        
+        Thread.sleep(1000);
         System.out.println("Greetings "+pn.playerName+" welcome to (town name) where rookie"
                 + " adventurers such as yourself come to start their journey!");
+        Thread.sleep(2500);
+        System.out.println("(where would like to go?) \n[1]Adventurers guild"
+                + "\n[2]Blacksmith \n[3]Potion seller \n[4]The plains \n[5]Inventory"
+                + "\n[6] Save your game");
+        
         
     }
     
