5d3c8e16ebe3d88b46b2878c3b7b3fe757a8daae
ytp0938 <163087832+ytp0938@users.noreply.github.com>
2024-04-09 14:18:13 +1200

    Add files via upload

---
+++ Combat.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdcproject;
+
+/**
+ *
+ * @author Uni
+ */
+public class Combat
+{
+    //is called when the player fights an enemy
+}
