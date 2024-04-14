e6d142d9f12261af9714d7ec52c5f5e8ac90e896
ytp0938 <163087832+ytp0938@users.noreply.github.com>
2024-04-14 14:14:16 +1200

    Update Level.java

--- Level.java
+++ Level.java
@@ -8,7 +8,48 @@ package pdcproject;
  *
  * @author Uni
  */
-public class Level
+public class Level implements experiencePoints
 {
+    private int level;
+    private int currentEXP;
     
+    public Level()
+    {
+        this.currentEXP = 0;
+        this.level = 1;
+    }
+    
+    @Override
+    public int getCurrentEXP() 
+    {
+        return currentEXP;
+    }
+
+    @Override
+    public int getLevel() 
+    {
+        return level;
+    }
+    
+    private void levelUp()
+    {
+        level++;  
+        System.out.println("You are now level "+level+"!");
+    }
+
+    @Override
+    public void gainEXP(int EXP)
+    {
+        currentEXP += EXP;
+        
+        while(currentEXP >= levelReq())
+        {
+            levelUp();
+        }
+    }
+    
+    private int levelReq()
+    {
+        return level * 50;
+    }
 }
