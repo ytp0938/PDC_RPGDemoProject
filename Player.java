1c4f006e3fa9df92c8a81990ba642993a16e3212
ytp0938 <163087832+ytp0938@users.noreply.github.com>
2024-04-14 14:20:05 +1200

    Update Player.java

--- Player.java
+++ Player.java
@@ -8,7 +8,66 @@ package pdcproject;
  *
  * @author Uni
  */
-public class Player 
+public class Player extends Level implements healthPoints
 {
-    String playerName;
+    private String playerName;
+    private int hp = 100;
+    private Level playerLevel;
+    
+    //player stats
+    public Player(String playerName, int hp)
+    {
+        this.playerName = playerName;
+        this.hp = hp;
+        this.playerLevel = new Level();
+    }
+
+    //empty constructor
+    public Player() 
+    {
+    }
+    
+    public String getPlayerName()
+    {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName)
+    {
+        this.playerName = playerName;
+    }
+
+    @Override
+    public int getHP() 
+    {
+        return hp;
+    }
+    
+    public int getPlayerLevel()
+    {
+        return playerLevel.getLevel();
+    }
+
+    @Override
+    public void Damage(int dmg) 
+    {
+        hp -= dmg;
+        if(hp <= 0)
+        {
+            hp = 0;
+            System.out.println("Game Over!");
+        }
+    }
+
+    @Override
+    public boolean isAlive() 
+    {
+        return hp > 0;
+    }
+    
+    @Override
+    public void gainEXP(int EXP)
+    {
+        playerLevel.gainEXP(EXP);
+    }
 }
