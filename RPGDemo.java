87a4a12521679f86a67a87dcc7d1f37990421af0
ytp0938 <163087832+ytp0938@users.noreply.github.com>
2024-04-14 20:47:14 +1200

    Update RPGDemo.java

--- RPGDemo.java
+++ RPGDemo.java
@@ -13,14 +13,11 @@ import java.util.Scanner;
  */
 
 /*TO-DO LIST
-finish dialogue trees for all player scenarios 
-create an additional method for when the player finishes the demo(when the player progress is equal to two) 
-thanking them for playing the game and also asking if they would like to save, then the program will close
 add in unimplemented classes
 */
 public class RPGDemo 
 {
-    Player pn = new Player();
+    private final Player pn = new Player();
     private int playerProgress = 0; //depending on how far the player has progressed the counter will increment by one
     private boolean questStarted = false; //once the player has started the quest it will become true
     private final Scanner scan = new Scanner(System.in);
@@ -81,6 +78,7 @@ public class RPGDemo
                         pause(5);
                         System.out.println("just head on over to 'The plains' to get started on you quest!");
                         playerProgress += 1;
+                        System.out.println("(you have started quest "+1+")");
                         questStarted = true;
                         pause(5);
                         System.out.println("\n\n\n\n");
@@ -96,8 +94,26 @@ public class RPGDemo
                     }
                     else if(playerProgress == 2)
                     {
-                        System.out.println("(insert poorly written dialogue here)");
-                        //will send the player to the "thanks for playing" method
+                        System.out.println("Hey! look at you! it seems that you've gotten stronger!");
+                        pause(2);
+                        System.out.println("Must mean that you've finished you quest!");
+                        pause(2);
+                        System.out.println("With that I can make you a member of the guild!"
+                                + " just sign your name right here!");
+                        System.out.print(".");
+                        pause(1);
+                        System.out.print(".");
+                        pause(1);
+                        System.out.print(".\n");
+                        pause(2);
+                        System.out.println("(you became a member of the Faba Adventurers Guild!)");
+                        pause(2);
+                        System.out.println("Well then "+pn.getPlayerName()+". I wish you the best of luck "
+                                + "on your journey!");
+                        pause(3);
+                        System.out.println("See ya around kid!\n\n\n\n\n\n");
+                        pause(2);
+                        thanksForPlaying();
                     }
                     break;
                 case 2:
@@ -118,16 +134,22 @@ public class RPGDemo
                         pause(2);
                         townSquare();
                     }
-                    else if(questStarted == true)
+                    else if(playerProgress == 1)
                     {
-                        System.out.println("battle with enemy");
-                        playerProgress+=1;
+                        Player player = new Player(pn.getPlayerName(), 30);
+                        KoboldEnemy kobold = new KoboldEnemy();
+                        Combat c = new Combat(player, kobold);
+                        c.playerCombat();
+                        if (!player.isAlive()) 
+                        {
+                            System.out.println("YOU DIED!\nGAME OVER");
+                            return;
+                        }
+                        
+                        playerProgress += 1;
+                        System.out.println("\nQuest Complete!");
+                        questStarted = false;
                         townSquare();
-                    }
-                    else if(playerProgress == 2)
-                    {
-                        System.out.println("monster has been dealt with");
-                        //need to update dialogue to recommend player to go to the guild
                     }
                     break;
                 case 4:
@@ -165,5 +187,13 @@ public class RPGDemo
         {
             System.out.println(e.getLocalizedMessage());
         }
+    }
+    
+    public void thanksForPlaying()
+    {
+        System.out.println("This is the end of our RPG Demo! I hope that you enjoyed"
+                + " our game!");
+        pause(3);
+        System.out.println("THE END!");
     }
 }
