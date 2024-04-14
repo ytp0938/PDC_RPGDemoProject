9f62483c7334eb8da8e11d5b35aa1b0a90098dfd
ytp0938 <163087832+ytp0938@users.noreply.github.com>
2024-04-14 20:48:13 +1200

    Add files via upload

---
+++ enemyDefeated.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdcproject;
+
+/**
+ *
+ * @author Uni
+ */
+interface enemyDefeated 
+{
+    public int getDroppedEXP();
+    public String getDroppedItems();
+}
