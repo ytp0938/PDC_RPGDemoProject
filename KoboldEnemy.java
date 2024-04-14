9ef9ab1f6f4385b0be92785188e067e9a0ac153d
ytp0938 <163087832+ytp0938@users.noreply.github.com>
2024-04-14 14:16:57 +1200

    Update KoboldEnemy.java

--- KoboldEnemy.java
+++ KoboldEnemy.java
@@ -8,6 +8,43 @@ package pdcproject;
  *
  * @author Uni
  */
-public class KoboldEnemy {
+public class KoboldEnemy implements healthPoints, enemyDefeated
+{
+    private int hp = 50;
+    private final int droppedEXP = 50;
+    private String droppedItems = "healthPotion";
     
+    @Override
+    public int getHP() 
+    {
+        return hp;
+    }
+        
+    @Override
+    public int getDroppedEXP() 
+    {
+        return droppedEXP;
+    }
+
+    @Override
+    public String getDroppedItems() 
+    {
+        return droppedItems;
+    }
+
+    @Override
+    public void Damage(int dmg) 
+    {
+        hp -= dmg;
+        if(hp < 0)
+        {
+            hp = 0;
+        }
+    }
+
+    @Override
+    public boolean isAlive() 
+    {
+        return hp > 0;
+    }      
 }
